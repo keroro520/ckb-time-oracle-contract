9c289f714387c90e6d4bd42e86e1fbd6708a041b
keroro <keroroxx520@gmail.com>
2024-04-17 14:27:12 +0800

    feat: add types.mol

--- contracts/time-oracle/src/main.rs
+++ contracts/time-oracle/src/main.rs
@@ -1,6 +1,11 @@
 #![no_std]
 #![cfg_attr(not(test), no_main)]
 
+// define modules
+mod entry;
+mod error;
+mod types;
+
 #[cfg(test)]
 extern crate alloc;
 
@@ -9,10 +14,12 @@ use ckb_std::default_alloc;
 #[cfg(not(test))]
 ckb_std::entry!(program_entry);
 #[cfg(not(test))]
-default_alloc!();
+default_alloc!(6 * 1024, 3000 * 1024, 64);
 
+/// program entry
 pub fn program_entry() -> i8 {
-    ckb_std::debug!("This is a sample contract!");
-
-    0
+    match entry::main() {
+        Ok(_) => 0,
+        Err(err) => err as i8,
+    }
 }
